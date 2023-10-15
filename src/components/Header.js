374250fa206fde6cf1ffc8f7aa99b20419e8df67
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-10-15 15:30:14 +0530

    Login component updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from "../assets/netflix-logo.png";
 
 const Header = () => {
   return (
-    <div className="absolute z-10">
+    <div className="absolute z-10 p-5">
       <img className="w-44 " src={Logo} alt="logo" />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,12 +4,40 @@ import BackgroundImg from "../assets/netflix-bg.jpg";
 
 const Login = () => {
   return (
-    <div>
+    <div className="">
       <Header />
-      <div className="">
-        <img className="bg-contain" src={BackgroundImg} alt="bg-img" />
+      <div>
+        <img
+          className="absolute  bg-image  w-screen bg-cover bg-center "
+          src={BackgroundImg}
+          alt="bg-img"
+        />
       </div>
-      <div className="absolute inset-0 bg-slate-900 bg-opacity-50"></div>
+      <div className="absolute inset-0 bottom-[-112px]  bg-slate-900 bg-opacity-70"></div>
+      <div className="text-white py-10  bg-black absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-opacity-75 rounded-xl">
+        <form className="min-w-[450px] px-20 py-5 flex flex-col">
+          <h1 className="text-4xl font-semibold mb-6">Sign In</h1>
+          <input
+            className=" px-4 py-4 mb-5 bg-gray-700 rounded-md "
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+            className="px-4 py-4 mb-5 bg-gray-700 rounded-md "
+            type="password"
+            placeholder="password"
+          />
+          <button className="px-4 py-4 mb-5 font-medium bg-red-700 rounded-md ">
+            Sign In
+          </button>
+          <p className="pt-8 text-gray-400">
+            New to Netflix?{" "}
+            <span className="cursor-pointer font-semibold text-white">
+              Sign Up Now
+            </span>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
