9d195571d600123d6888d2860f59fae70a6b8b03
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-10-15 23:28:17 +0530

    added state to toggle to Sign Up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import BackgroundImg from "../assets/netflix-bg.jpg";
 
 const Login = () => {
+  const [isUser, setIsUser] = useState(true);
+
+  const handleToggle = () => {
+    setIsUser(!isUser);
+    console.log(isUser);
+  };
+
   return (
     <div className="">
       <Header />
@@ -16,7 +23,16 @@ const Login = () => {
       <div className="absolute inset-0 bottom-[-112px]  bg-slate-900 bg-opacity-70"></div>
       <div className="text-white py-10  bg-black absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-opacity-75 rounded-xl">
         <form className="min-w-[450px] px-20 py-5 flex flex-col">
-          <h1 className="text-4xl font-semibold mb-6">Sign In</h1>
+          <h1 className="text-4xl font-semibold mb-6">
+            {isUser ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isUser && (
+            <input
+              className=" px-4 py-4 mb-5 bg-gray-700 rounded-md "
+              type="text"
+              placeholder="Full Name"
+            />
+          )}
           <input
             className=" px-4 py-4 mb-5 bg-gray-700 rounded-md "
             type="text"
@@ -25,15 +41,18 @@ const Login = () => {
           <input
             className="px-4 py-4 mb-5 bg-gray-700 rounded-md "
             type="password"
-            placeholder="password"
+            placeholder="Password"
           />
           <button className="px-4 py-4 mb-5 font-medium bg-red-700 rounded-md ">
-            Sign In
+            {isUser ? "Sign In" : "Sign Up"}
           </button>
           <p className="pt-8 text-gray-400">
-            New to Netflix?{" "}
-            <span className="cursor-pointer font-semibold text-white">
-              Sign Up Now
+            {isUser ? "New to Netflix? " : "Already a user? "}
+            <span
+              onClick={handleToggle}
+              className="cursor-pointer font-semibold text-white"
+            >
+              {isUser ? "Sign Up" : "Sign In"}
             </span>
           </p>
         </form>
