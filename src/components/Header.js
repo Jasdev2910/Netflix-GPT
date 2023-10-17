cc5efeca713f5f28eaffc11820a1eff170bb224f
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-10-17 18:03:29 +0530

    material ui added & updating and dispatching

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/netflix-logo.png";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  ListItem,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
+import { Logout } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const user = useSelector((store) => store.user);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className="absolute z-10 p-5">
+    <div className="w-full  flex justify-between absolute z-10 p-5">
       <img className="w-44 " src={Logo} alt="logo" />
+      {user && (
+        <div>
+          <Box>
+            <Tooltip>
+              <IconButton onClick={handleClick}>
+                <Avatar className="bg-orange-500">
+                  {user.displayName.charAt(0)}
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
+
+          {isOpen && (
+            <div className="bg-white w-[130px] right-10 absolute rounded-md">
+              <ListItem
+                onClick={handleSignOut}
+                className="cursor-pointer hover:bg-slate-300 hover:rounded-md"
+              >
+                <Logout />
+                Logout
+              </ListItem>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,15 +5,20 @@ import { checkValidData } from "../utils/validation";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isUser, setIsUser] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
@@ -38,8 +43,28 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://example.com/jane-q-user/profile.jpg",
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.message);
+            });
           console.log(user);
-          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -94,6 +119,7 @@ const Login = () => {
           </h1>
           {!isUser && (
             <input
+              ref={name}
               className=" px-4 py-4 mb-5 bg-gray-700 rounded-md "
               type="text"
               placeholder="Full Name"
