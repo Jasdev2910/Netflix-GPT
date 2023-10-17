649a2b1879ce0d5da4177c41dfa194663caf8a53
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-10-18 01:37:42 +0530

    fetched API & added data to store

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDE5NDMzOWFjZDA4ZDFmYzBmNmEyOTBkNjMzNWYwMSIsInN1YiI6IjY1MmViY2EwZWE4NGM3MDEwYzFkOWQ3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q_SQqWic8RIVS-YcyoXOMSy4hRfbJ0AqatwjHD_OF8E",
+  },
+};
