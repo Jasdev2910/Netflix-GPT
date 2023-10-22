8938109dde02d372e18dbdb81ff7619d42edbf5f
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-10-22 19:32:33 +0530

    language config added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,9 @@ export const API_OPTIONS = {
 };
 
 export const IMAGE_CDN_URL = "https://image.tmdb.org/t/p/w500/";
+
+export const SUPPORTED_LANG = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
