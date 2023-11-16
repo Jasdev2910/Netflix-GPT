bee2337cbc6e0b9d28015ca09c9940551bf024f2
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-11-16 17:00:23 +0530

    fallback image added

--- src/components/CastCard.js
+++ src/components/CastCard.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { IMAGE_CDN_URL } from "../utils/constants";
+import Avatar from "../assets/avatar.png";
 
 const CastCard = ({ img_path, name, character }) => {
   return (
-    <div className="w-[200px] h-[150px] text-black">
-      <div className="w-52 ">
-        <img src={IMAGE_CDN_URL + img_path} alt="cast-img" />
+    <div className="text-black m-3 shadow-xl rounded-xl">
+      <div className="w-[120px] object-cover">
+        {img_path !== null ? (
+          <img
+            className="rounded-t-lg "
+            src={IMAGE_CDN_URL + img_path}
+            alt="cast-img"
+            loading="lazy"
+          />
+        ) : (
+          <img
+            className="rounded-t-lg object-cover"
+            src={Avatar}
+            alt="cast-img"
+          />
+        )}
       </div>
-      <div>
-        <h3>{name}</h3>
-        <p>{character}</p>
+      <div className="">
+        <h3 className="pl-2 text-lg font-semibold">{name}</h3>
+        <p className="text-sm  pl-2 pb-2">{character}</p>
       </div>
     </div>
   );
