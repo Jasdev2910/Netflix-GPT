84317ba794a71bbe60e0a1bd921ccb930319df8d
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-12-09 23:38:41 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Logo from "../assets/netflix-logo.png";
 
 const Footer = () => {
-  return <div className="h-[400px]">Footer</div>;
+  const date = new Date().getFullYear();
+  return (
+    <div className=" p-10 w-[100%] h-[190px] bg-[#343a40] ">
+      <div className="flex justify-evenly list-none text-gray-300">
+        <div className="">
+          <img className="w-36" src={Logo} alt="logo" />
+          <h1 className="py-2">
+            copyright © <br />
+            {date}
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-lg font-bold ">Company</h1>
+          <li className="hover:text-white">About</li>
+          <li className="hover:text-white">Team</li>
+
+          <li className="hover:text-white">Offers</li>
+        </div>
+        <div>
+          <h1 className="text-lg font-bold ">Contact Us</h1>{" "}
+          <li className="hover:text-white">Help & Support</li>
+          <li className="hover:text-white">Partner with us</li>
+          <li className="hover:text-white">Ride with us</li>
+        </div>
+        <div>
+          <h1 className="text-lg font-bold ">Legal</h1>
+          <li className="hover:text-white">Terms & Condition</li>
+          <li className="hover:text-white">Cookie Policy</li>
+          <li className="hover:text-white">Privacy Policy</li>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
